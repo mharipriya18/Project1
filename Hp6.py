74616da9c978028529d83bc6bc2e307cac6d51a8
mharipriya18 <143766757+mharipriya18@users.noreply.github.com>
2023-09-06 21:52:39 +0530

    Add files via upload

---
+++ Hp6.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a value 1:"))
+b=int(input("Enter b value 2:"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("The sum of a and b is:",c)
+print("The difference of a and b is:",d)
+print("The product of a and b is:",e)
+print("The Division of a and b is:",f)
